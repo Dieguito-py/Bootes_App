1acf02fa67a8a14abe10cc951271af39d427e10b
Dieguito-py <DiegoBernardi@protonmail.com>
2023-11-26 20:44:57 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,84 @@
 import 'package:bootes_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    late Future<void> _getData;
+  late Timer _timer;
+  late TextEditingController _controller;
+  String baseUrl = 'http://192.168.1.100'; // URL padrão
+  late String humidity = '';
+  late String temperature = '';
+  late String pressure = '';
+  late String altitude = '';
+  late String latitude = '';
+  late String longitude = '';
+
+  Future<void> fetchData(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      List<String> dataList = data.split("e");
+
+      if (endpoint == 'dados') {
+        for (int i = 0; i < dataList.length; i++) {
+          if (i == 0) {
+            temperature = dataList[i].split('.')[0];
+          } else if (i == 1) {
+            humidity = dataList[i].split('.')[0];
+          } else if (i == 2) {
+            pressure = dataList[i];
+          } else if (i == 3) {
+            altitude = dataList[i];
+          }
+        }
+      } else if (endpoint == 'gps') {
+        latitude = dataList[0].split('.')[0];
+        longitude = dataList[1].split('.')[0];
+      }
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getData = fetchData('dados');
+    _controller = TextEditingController();
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer t) {
+      setState(() {
+        _getData = fetchData('dados');
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void updateBaseUrl(String newUrl) {
+    setState(() {
+      baseUrl = newUrl;
+      _getData = fetchData('dados');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -25,14 +26,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<String>> _getData;
+  late Future<void> _getData;
+  late Timer _timer;
+  late TextEditingController _controller;
+  String baseUrl = 'http://192.168.1.100'; // URL padrão
+  late String humidity = '';
+  late String temperature = '';
+  late String pressure = '';
+  late String altitude = '';
+  late String latitude = '';
+  late String longitude = '';
 
-  Future<List<String>> fetchData() async {
-    final response = await http.get(Uri.parse('http://192.168.1.100/dados'));
+  Future<void> fetchData(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
     if (response.statusCode == 200) {
       String data = response.body;
       List<String> dataList = data.split("e");
-      return dataList;
+
+      if (endpoint == 'dados') {
+        for (int i = 0; i < dataList.length; i++) {
+          if (i == 0) {
+            temperature = dataList[i].split('.')[0];
+          } else if (i == 1) {
+            humidity = dataList[i].split('.')[0];
+          } else if (i == 2) {
+            pressure = dataList[i];
+          } else if (i == 3) {
+            altitude = dataList[i];
+          }
+        }
+      } else if (endpoint == 'gps') {
+        latitude = dataList[0].split('.')[0];
+        longitude = dataList[1].split('.')[0];
+      }
     } else {
       throw Exception('Failed to load data');
     }
@@ -41,7 +67,27 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    _getData = fetchData();
+    _getData = fetchData('dados'); // Começa obtendo os dados de 'dados'
+    _controller = TextEditingController();
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer t) {
+      setState(() {
+        _getData = fetchData('dados'); // Atualiza os dados de 'dados' a cada 5 segundos
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void updateBaseUrl(String newUrl) {
+    setState(() {
+      baseUrl = newUrl;
+      _getData = fetchData('dados'); // Atualiza os dados de 'dados' quando o endereço IP é alterado
+    });
   }
 
   @override
@@ -51,29 +97,59 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('BOOTES Flutter'),
       ),
       body: Center(
-        child: FutureBuilder<List<String>>(
-          future: _getData,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              List<String>? dataList = snapshot.data;
-              if (dataList != null && dataList.isNotEmpty) {
-                return ListView.builder(
-                  itemCount: dataList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text('Dado ${index + 1}: ${dataList[index]}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Enter IP Address'),
+                      content: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(hintText: 'Enter IP Address'),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            updateBaseUrl('http://${_controller.text}');
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
                     );
                   },
                 );
-              } else {
-                return Text('No data');
-              }
-            }
-          },
+              },
+              child: Text('Change IP Address'),
+            ),
+            SizedBox(height: 20),
+            FutureBuilder<void>(
+              future: _getData,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Column(
+                    children: [
+                      Text('Humidity: $humidity'),
+                      Text('Temperature: $temperature'),
+                      Text('Pressure: $pressure'),
+                      Text('Altitude: $altitude'),
+                      SizedBox(height: 20),
+                      Text('Latitude: $latitude'),
+                      Text('Longitude: $longitude'),
+                    ],
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
